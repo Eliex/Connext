d0c967af7d187b397f12b1567b756a13b3bf6345
jeanel <abarquez_elibar@yahoo.com>
2017-01-22 00:29:32 +0800

    adding some functions for layout

--- app/src/main/java/com/google/firebase/codelab/friendlychat/CodelabPreferences.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/CodelabPreferences.java
@@ -20,4 +20,6 @@ public class CodelabPreferences {
     public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
     public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
 
+    //public void function()
+
 }
